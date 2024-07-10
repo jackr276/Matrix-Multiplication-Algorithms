3cb5c68b08232fe617f3add828777fedd921241c
jackr276 <jack_m_robbins@me.com>
2024-07-10 19:03:07 -0400

    Update matrix_multiplication.py

--- src/matrix_multiplication.py
+++ src/matrix_multiplication.py
@@ -8,10 +8,16 @@ import numpy as np
 import time
 
 
+"""
+Multiply the two arrays the classic way, using a triple for loop
+"""
 def matrix_mult_classic(a, b):
     result = np.array([[0., 0., 0., 0.], [0., 0., 0., 0.], [0., 0., 0., 0.], [0., 0., 0., 0.]])
+    
+    #Start the timer
     start_time = time.time()
 
+    # Regular matrix multiplication using 3 for loops
     for i in range(len(a)):
         for j in range(len(a[0])):
             for k in range(len(b)):
@@ -19,28 +25,40 @@ def matrix_mult_classic(a, b):
 
     end_time = time.time()
 
+    # Rounding, not counted in execution time
     for i in range(len(a)):
         for j in range(len(a[0])):
             result[i][j] = round(result[i][j], 2)
 
+    #Return both execution result and runtime
     return (result, (end_time - start_time) * 1000000)
    
 
+"""
+Entry point function into strassen multiplication
+"""
 def matrix_mult_strassen(a, b): 
+    #Start and end the timer, letting the helper method handle things
     start_time = time.time()
     result = strassen_helper(a, b)
     end_time = time.time() 
 
+    #Rounding, not counted in execution time
     for i in range(len(a)):
         for j in range(len(a[0])):
            result[i][j] = round(result[i][j], 2)
 
+    #Return both execution result and runtime
     return (result, (end_time - start_time) * 1000000)
 
 
+"""
+Recursive helper function for Strassen
+"""
 def strassen_helper(a, b):
     n = len(a)
 
+    #Base case, simply do a regular 2 x 2 multiplication
     if n < 3:
         return np.dot(a, b)
 
@@ -58,7 +76,7 @@ def strassen_helper(a, b):
     Matrix_b_2_1 = b[middle:, :middle]
     Matrix_b_2_2 = b[middle:, middle:]
 
-    # Recursively multiply all submatrices
+    # Recursively multiply all combinations of submatrices
     Product_1 = strassen_helper(Matrix_a_1_1, Matrix_b_1_2 - Matrix_b_2_2)
     Product_2 = strassen_helper(Matrix_a_1_1 + Matrix_a_1_2, Matrix_b_2_2)
     Product_3 = strassen_helper(Matrix_a_2_1 + Matrix_a_2_2, Matrix_b_1_1)
@@ -66,14 +84,17 @@ def strassen_helper(a, b):
     Product_5 = strassen_helper(Matrix_a_1_1 + Matrix_a_2_2, Matrix_b_1_1 + Matrix_b_2_2)
     Product_6 = strassen_helper(Matrix_a_1_2 - Matrix_a_2_2, Matrix_b_2_1 + Matrix_b_2_2)
     Product_7 = strassen_helper(Matrix_a_1_1 - Matrix_a_2_1, Matrix_b_1_1 + Matrix_b_1_2)
-    
+   
+    # Combine multiplication results
     Matrix_c_1_1 = Product_5 + Product_4 - Product_2 + Product_6
     Matrix_c_1_2 = Product_1 + Product_2
     Matrix_c_2_1 = Product_3 + Product_4
     Matrix_c_2_2 = Product_5 + Product_1 - Product_3 - Product_7
 
+    # Reconstruct the matrix by vertically stacking the two halves, each of which are horizontally stacked
     C = np.vstack((np.hstack((Matrix_c_1_1, Matrix_c_1_2)), np.hstack((Matrix_c_2_1, Matrix_c_2_2))))
     return C
+
 
 """
 Main method for running/printing
@@ -86,12 +107,13 @@ def main():
     print(M)
     print("\n")
 
-    # Calculate inverse
+    # Calculate and print inverse
     N= np.linalg.inv(M)
     print("Inverse of M N:")
     print(N)
     print("\n")
 
+    #For each matrix multiplication result, print to console
     result_tuple = matrix_mult_classic(M, N)
     print("Result of M X N using classic matrix multiplication algorithm:")
     print(result_tuple[0])
@@ -116,5 +138,7 @@ def main():
     print("Operation completed in:", round(result_tuple[1], 5), " microseconds")
     print("\n")
 
+
+#Make this file a script
 if __name__ == '__main__':
     main()
